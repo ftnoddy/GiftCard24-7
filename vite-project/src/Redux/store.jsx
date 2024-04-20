11c4e67f00b9cd48a461edc86db0bdcc841297b9
Atindra Mohan das <atindramohandas353@gmail.com>
2024-04-20 13:37:05 +0530

    fixing

--- vite-project/src/Redux/store.jsx
+++ vite-project/src/Redux/store.jsx
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./Slices/cartSlice";
+import authSlice from "./Slices/authSlice";
 
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
+    auth: authSlice,
   },
 });
