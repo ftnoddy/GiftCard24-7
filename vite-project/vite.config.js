669853d71f4f97d85c87d446509bd4c606929291
Atindra Mohan das <atindramohandas353@gmail.com>
2024-04-20 14:06:04 +0530

    Vite fix

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+server: {
+  host: true
+}
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
