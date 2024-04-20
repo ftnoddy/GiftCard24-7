c5c6f2e4ca40714170d0cb5ecdd3132e9ac87a66
Atindra Mohan das <atindramohandas353@gmail.com>
2024-04-20 13:53:37 +0530

    port fix

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 connectDB();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5002;
 
 //Body parser Middleware
 app.use(express.json());
