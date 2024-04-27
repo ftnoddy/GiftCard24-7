1c1b692daecfd34a6693a7dcbd8f278e7777a315
Ayush <ayushmishra2854@gmail.com>
2024-04-27 18:12:28 +0530

    Reverted accidental removal from cache

--- Backend/routes/authRoutes.jsx
+++
@@ -1,6 +0,0 @@
-// import { Router } from 'express';
-// import { kyc } from '../controllers/authController';
-// const router = Router();
-
-// router.post('/kyc', kyc);
-// export default router;
--- Backend/routes/otpRoutes.jsx
+++
@@ -1,5 +0,0 @@
-// import { Router } from 'express';
-// import { sendOTP } from '../controllers/otpController';
-// const router = Router();
-// router.post('/send-otp', sendOTP);
-// export default router;
