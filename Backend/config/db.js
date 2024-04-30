f95fc3dc12f702ce4b7ae68cf2feb4daf47a71bb
Atindra Mohan das <atindramohandas353@gmail.com>
2024-04-30 12:12:18 +0530

    db fix

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,17 +1,9 @@
 import mongoose from "mongoose";
 import colors from "colors";
 
-
-// Load environment variables from .env file
-
-
 const connectDB = async () => {
   try {
-    const DB = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      // serverSelectionTimeoutMS: 5000, // Set a higher value (e.g., 5000 milliseconds)
-    });
+    const DB = await mongoose.connect(process.env.MONGO_URI);
     console.log(`Connected to MongoDB with ${DB.connection.host}`.green.inverse);
   } catch (error) {
     console.log(`Error: ${error.message}`.red.inverse);
